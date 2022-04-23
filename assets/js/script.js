268488fe25b17530dfe72638810231b1fbc7bebf
H0PE467 <78632803+H0PE467@users.noreply.github.com>
2022-04-23 16:35:31 -0500

    Open Weather API Implementation
    Creates New Buttons on History
    Asks for info to the weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,22 @@ function startButtonSearch(event) {
 	getLatLon(location);
 }
 
+function addToHistory(city){
+	newBtn = document.createElement("button");
+	newBtn.setAttribute("type","button");
+	newBtn.textContent = capitalize(city);
+	historySearch.prepend(newBtn);
+	newBtn.addEventListener("click",startButtonSearch)
+}
+
+async function getWeatherInfo(lat,long) {
+	let weatherObj = await fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=current,minutely,hourly&appid=25290662f09e415ae65ed4015f527868");
+	let weatherText = await weatherObj.text();
+	let	weatherJSON = JSON.parse(weatherText);
+	console.log(weatherJSON);
+	setNextFiveCards(weatherJSON);
+	setCurrentWeather(weatherJSON);
+}
 
 
 searchBtn.addEventListener("click", startSearch);
